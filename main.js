3e98c8865241e74bf26c6c0b698e6d7241c508f4
LokankshiSharma <81632228+LokankshiSharma@users.noreply.github.com>
2023-07-31 10:13:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,23 @@
 function setup(){
     canvas=createCanvas(500, 500);
  canvas.position(400,90);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
   }
   function draw(){
     textSize(10);
     fill('FFE787');
-    text('Hello World, 50, 400')
+    text('Hello World', 50, 400);
   }
-  function gotPoses(Results){
+  function gotPoses(results){
     if(results.length > 0){
       console.log(results);
     leftWristX= results[0].pose.leftWrist.x;
     rightWristX= results[0].pose.rightWrist.x;
-    difference = floor (leftWristX - rightWristX);
-  }
+    difference = floor (leftWristX - rightWristX);  
   }
   function modelLoaded(){
     console.log("PoseNet is initialized");
-  }
-  function gotResult(results){
-  if (results.length>0)
-  {
-  console.log(results)
-  }
   }
 function modelLoaded(){
   console.log("PoseNet is initialized");
@@ -31,14 +26,6 @@ function gotResult(results){
 if (results.length>0)
 {
 console.log(results)
-noseX=results[0].pose.nose.x;
-noseY=results[0].pose.nose.y;
-console.log("noseX="+noseX+ " ,noseY=" + noseY);
-rightX=results[0].pose.rightWrist.x;
-leftX=results[0].pose.leftWrist.x;
-difference=floor(rightX-leftX);
-console.log("rightX"+rightX+ ", leftX=" + leftX + "difference" + difference)
-}
 }
 leftWristX=0;
 rightWristX=0;
