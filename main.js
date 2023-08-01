b6917d5414a8e062cd6f6c7204ef5eea718ee453
LokankshiSharma <81632228+LokankshiSharma@users.noreply.github.com>
2023-08-01 18:15:20 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,8 +3,7 @@ function setup(){
  canvas.center();
       video=createCapture( VIDEO );
   video.size(500, 500);
-  PoseNet=ml5.poseNet(video, modelLoaded);
-  PoseNet.on("pose", gotResult)
+ 
   poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
   }
@@ -24,11 +23,6 @@ function setup(){
   function modelLoaded(){
     console.log("PoseNet is initialized");
   }
-function gotResult(results){
-if (results.length>0)
-{
-console.log(results)
-}
 leftWristX=0;
 rightWristX=0;
 difference=0; 
